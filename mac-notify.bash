7bdfddff253417b9b12095b2bf5b0ec04176f7b4
Chris Guerra <chris@methodsmb.com>
2017-07-10 13:19:15 -0400

    Update mac-notify.bash

--- mac-notify.bash
+++ mac-notify.bash
@@ -3,7 +3,6 @@ function notification() {
   osascript <<EOT
     tell app "System Events"
       display dialog "$1" buttons {"OK"} default button 1 with icon note with title "$2"
-      return
     end tell
 EOT
 }
